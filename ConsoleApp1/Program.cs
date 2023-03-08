557d76a8c451d0bd485dcf945999c4c5c9f4e0e6
yuiver <dbals1523@gmail.com>
2023-03-08 11:27:47 +0900

    [yuiver]Project init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+        }       //Main(string[] args)
+    }       //Class Program
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54F;
+        public int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            private set { SetInch(value); }
+        }       //float Inch
+        public Ruler(int cmValue) { Centimeter = cmValue; }
+        public void Run()
+        {
+            Console.WriteLine($"{Centimeter}cm 는 {Inch}inch 입니다.");
+        }       //Run()
+        private void SetInch(float InchValue)
+        {
+            Centimeter = (int)(InchValue / ONE_INCH);
+        }       //SetInch()
+    }       //Class Ruler
+}
