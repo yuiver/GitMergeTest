87c0a49a786299a72792bfccfd30b41ac7b25480
yuiver <dbals1523@gmail.com>
2023-03-08 11:55:50 +0900

    feat[gamma]2023. 03. 09 Add new Class IamGamma /Gamma

---
+++ ConsoleApp1/IamGamma.cs
@@ -0,0 +1,14 @@
+﻿    //      {   2023. 03. 09 Add new Class IamGamma /Gamma
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class IamGamma
+    {
+    }
+}
+//      {   2023. 03. 09 Add new Class IamGamma /Gamma
